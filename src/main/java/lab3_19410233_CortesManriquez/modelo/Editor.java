38ee99dd6b2294ad33b0a3db930c4d74f6df795b
Matias_ubuntu <matias.cortes@usach.cl>
2022-01-10 22:58:20 -0300

    Inicio clase editor

---
+++ src/main/java/lab3_19410233_CortesManriquez/modelo/Editor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab3_19410233_CortesManriquez.modelo;
+
+/**
+ *
+ * @author matias
+ */
+public class Editor {
+    
+}
