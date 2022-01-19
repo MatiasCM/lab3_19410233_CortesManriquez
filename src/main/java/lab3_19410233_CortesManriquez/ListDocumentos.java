4bbcd4e22a9788b08e6c52946708c5a9c98330cf
Matias_ubuntu <matias.cortes@usach.cl>
2022-01-19 13:55:14 -0300

    Clase ListDocumentos

---
+++ src/main/java/lab3_19410233_CortesManriquez/ListDocumentos.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab3_19410233_CortesManriquez;
+
+import java.util.List;
+
+/**
+ *
+ * @author matias
+ */
+public class ListDocumentos {
+    public List<Documento> documentos;
+    public int cantidadDocumentos;
+    
+    //---Constructor---//
+    public ListDocumentos(List<Documento> documentos, int cantidadDocumentos) {
+        this.documentos = documentos;
+        this.cantidadDocumentos = cantidadDocumentos;
+    }
+    
+    //---Selectores---//
+    public List<Documento> getDocumentos() {
+        return documentos;
+    }
+    public int getCantidadDocumentos() {
+        return cantidadDocumentos;
+    }
+    
+    //---Modificadores---//
+    public void setDocumentos(List<Documento> documentos) {
+        this.documentos = documentos;
+    }
+    public void setCantidadDocumentos(int cantidadDocumentos) {
+        this.cantidadDocumentos = cantidadDocumentos;
+    }
+      
+}
